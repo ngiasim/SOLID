24c7b49f7272a4c6bf95fcce423954e15035dc5e
ngiasim <31400312+ngiasim@users.noreply.github.com>
2017-09-13 14:39:58 +0500

    Update SRP.php

--- SRP.php
+++ SRP.php
@@ -1,16 +1,39 @@
+<?php
 /**
  * An example of a Class that violates the Single Responsibility Principle.
  * Creation of customer and order mixed up.
  */
 
+
 // Bad Example
 class CustomerOrder {
      
-    public void createCustomer(int customerId, String name) {
+    public function createCustomer(Request $request) {
         // Create customer
     }
      
-    public void submitOrder(Cart shopCart) {
+    public function submitOrder(Request $request) {
         // Submit Orders for processing
     }
 }
+
+
+
+// Good Example
+class Customer {
+     
+    public function createCustomer(Request $request) {
+        // Create customer
+    }
+     
+}
+
+class Order {
+     
+    public function submitOrder(Request $request) {
+        // Submit Orders for processing
+    }
+     
+}
+
+?>
